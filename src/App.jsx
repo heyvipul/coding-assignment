e85ff4ff8cb2ca866d5f3e8063cacd21598326ee
heyvipul <vipulgirhestar@gmail.com>
2024-02-06 14:09:03 +0530

    frame3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Frame1 from './components/Frame1'
 import Frame2 from './components/Frame2'
+import Frame3 from './components/Frame3'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Frame1/>
       <Frame2/>
+      <Frame3/>
       <Footer/>
     </>
   )
---
+++ src/components/Frame3.jsx
@@ -0,0 +1,31 @@
+import "../components-css/frame3.css"
+
+const Frame3 = () => {
+  return (
+    <div className="frame3">
+    
+        <div>
+          <h3>Our Partners</h3>
+        </div>
+        <div>
+          <img src="/public/frame2/hdfc.png" alt="" />
+          <p>HDFC Bank</p>
+        </div>
+        <div>
+          <img src="/public/frame2/icic.png" alt="" />
+          <p>ICIC Bank</p>
+        </div>
+        <div>
+          <img src="/public/frame2/icic.png" alt="" />
+          <p>ICIC Bank</p>
+        </div>
+        <div>
+          <img src="/public/frame2/backofbaroda.png" alt="" />
+          <p>Bank of Baroda</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Frame3
