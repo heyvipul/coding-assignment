a0044a90a70c71b62ba86eaac030e61f6188bcf0
heyvipul <vipulgirhestar@gmail.com>
2024-02-06 14:50:26 +0530

    frame4

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Frame1 from './components/Frame1'
 import Frame2 from './components/Frame2'
 import Frame3 from './components/Frame3'
+import Frame4 from './components/Frame4'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
 
@@ -14,6 +15,7 @@ function App() {
       <Frame1/>
       <Frame2/>
       <Frame3/>
+      <Frame4/>
       <Footer/>
     </>
   )
---
+++ src/components/Frame4.jsx
@@ -0,0 +1,26 @@
+import "../components-css/frame4.css"
+
+const Frame4 = () => {
+  return (
+    <div className="frame4">
+        <div className="div1">
+            <img src="/public/frame4/mobile.png" alt="" />
+        </div>
+        <div className="div2">
+            <div className="lighting">
+                <img src="/public/frame4/Star.png" alt="" />
+                <p className="pp">Lightning Fast Process</p>
+            </div>
+            <div className="line">
+                <img src="/public/frame4/line.png" alt="" />
+            </div>
+            <div>
+                <p>Customer Share Basic Details</p>
+            </div>
+            <button className="btnnow">Apply Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Frame4
