96016a2d865a8e4341f84a3bde346d26cf84e823
heyvipul <vipulgirhestar@gmail.com>
2024-02-06 16:13:37 +0530

    frame5

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Frame1 from './components/Frame1'
 import Frame2 from './components/Frame2'
 import Frame3 from './components/Frame3'
 import Frame4 from './components/Frame4'
+import Frame5 from './components/Frame5'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
 
@@ -16,6 +17,7 @@ function App() {
       <Frame2/>
       <Frame3/>
       <Frame4/>
+      <Frame5/>
       <Footer/>
     </>
   )
---
+++ src/components/Frame5.jsx
@@ -0,0 +1,32 @@
+import "../components-css/frame5.css"
+
+const Frame5 = () => {
+  return (
+    <div className="frame5">
+        <div className="heading">
+            <img src="/public/frame5/Hand Heart.png" alt="" />
+            <p>What Our Customer Says</p>
+        </div>
+
+        <div className="flex-box">
+            <div className="man-png">
+                <img src="/public/frame5/man.png" alt="" />
+            </div>
+            <div >
+                <div><img src="/public/frame5/comma.png" alt="" /></div>
+                <div className="div2">
+                    <h3>SUNIL GUPTA</h3>
+                    <p>Chartered Accountant</p>
+                    <label> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sed quisquam sunt dolor nemo minima repudiandae ex itaque hic! Tempore repudiandae aperiam id pariatur ea, iste possimus eum culpa architecto eius veniam quasi at veritatis. Inventore, nulla architecto nisi omnis distinctio ipsum nobis provident fugiat minima, explicabo, deleniti aliquid praesentium. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid voluptatum, repellat voluptates magnam numquam repellendus accusantium modi, libero est assumenda, tempore amet at? Suscipit assumenda odit quam autem sapiente atque saepe culpa perferendis, fuga quidem voluptates impedit earum hic sed, officia tenetur optio omnis facilis iure? Numquam molestias non accusantium.</label>
+                    <div>
+                        <img src="/public/frame5/line.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+  )
+}
+
+export default Frame5
