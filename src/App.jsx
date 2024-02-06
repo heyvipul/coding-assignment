a1c144b9bad608905fa63401352418edf1f513b3
heyvipul <vipulgirhestar@gmail.com>
2024-02-06 13:40:17 +0530

    frame2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Frame1 from './components/Frame1'
+import Frame2 from './components/Frame2'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Navbar/>
       <Frame1/>
+      <Frame2/>
       <Footer/>
     </>
   )
---
+++ src/components/Frame2.jsx
@@ -0,0 +1,52 @@
+import "../components-css/frame2.css"
+
+const Frame2 = () => {
+  return (
+    <div className="frame2">
+        <div className="heading">
+            <img src="/public/frame2/Star.png" alt="" />
+            <h2>Personal Loan Highlights</h2>
+        </div>
+        <div className="frame2-next">
+            <div className="handsome-guy">
+                <img src="/public/frame2/handsome-guy.png" alt="handsome-guy" />
+            </div>
+            <div className="intrest-group">
+                <div className="intrest-div">
+                    <div>
+                        <div>
+                            <img src="/public/frame2/star 1.png" alt="" />
+                            <p>Intrest Rate</p>
+                        </div>
+                            <p>Usually <span>10.49% p.a</span> onwards: some PSUs may offer lower rates</p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="/public/frame2/star 1.png" alt="" />
+                            <p>Laon Amount</p>
+                        </div>
+                            <p>Can go up to <span>RS 40 lakh</span>some lenders may offer higher loan amount</p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="/public/frame2/star 1.png" alt="" />
+                            <p>Processing Fees</p>
+                        </div>
+                            <p><span>0.5% to 4% </span>of laon amount (may vary across lenders)</p>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="/public/frame2/star 1.png" alt="" />
+                            <p>Tenure</p>
+                        </div>
+                            <p>Up to<span>5 years</span> (some lenders offer repayment period till 8 year)</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Frame2
